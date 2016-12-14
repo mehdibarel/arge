c4b09d927107968c6627f584e5f45b75526269af
mehdibarel <mehdibarel@mehdibarel-HP-Pavilion-dv6000-RP948EA-AB8>
2016-12-14 23:35:39 +0200

    .

--- SpringbootJPADemo/src/main/java/sample/data/jpa/web/SampleController.java
+++ SpringbootJPADemo/src/main/java/sample/data/jpa/web/SampleController.java
@@ -16,15 +16,17 @@
 
 package sample.data.jpa.web;
 
-import sample.data.jpa.service.CityService;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import sample.data.jpa.service.CityService;
+
 @Controller
+@EnableAutoConfiguration
 public class SampleController {
 
 	@Autowired
